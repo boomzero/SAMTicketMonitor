33131ec7dfab7cad6d788c4a21a705a8cc607a31
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2023-04-28 16:48:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import time
+
+headers = {'Host': 'ticket.sstm.org.cn', 'Connection': 'keep-alive',
+           'mpsessid': '',
+           'content-type': 'application/x-www-form-urlencoded', 'Accept-Encoding': 'gzip,compress,br,deflate',
+           'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.32(0x18002033) NetType/3G Language/zh_CN',
+           'Referer': 'https://servicewechat.com/wx1d7ddce169710ba7/42/page-frame.html'}
+url = 'https://ticket.sstm.org.cn/vendor/reserve/getReservePeriodListByDate.xhtml?stadiumId=69001&reservedate=2023-04-28&appId=wx1d7ddce169710ba7'
+while True:
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        print(r.text)
+        print('request failed!')
+        time.sleep(20)
+    else:
+        data = json.loads(r.text)
+        if not data['success']:
+            print('request failed!')
+            print(r.text)
+            print("rate limited! waiting 20 seconds")
+            time.sleep(20)
+        else:
+            if data['data']['reservePeriodList'][0]['avaiablenum'] > 0 or data['data']['reservePeriodList'][1][
+                'avaiablenum'] > 0 or data['data']['reservePeriodList'][0]['availableNum'] > 0 or \
+                    data['data']['reservePeriodList'][1]['availableNum'] > 0:
+                requests.get('https://api.day.app/<barkToken>/有票了')
+                print(time.asctime(time.localtime(time.time())), " ticket available")
+            else:
+                print(time.asctime(time.localtime(time.time())), " ticket unavailable")
+            time.sleep(5)
