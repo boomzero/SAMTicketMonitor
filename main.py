0549020357a03237c1f2a46a18dc4a06d30b2736
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2023-04-30 08:06:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import requests
 import json
 import time
 
+mpsessid = '<mpsessid>'
+if mpsessid == '<mpsessid>':
+    print("Please fill out the mpsessid")
+    exit(1)
+barkUrl = 'https://api.day.app/6RjyBp6nrSfg8Y8sra4W9c/有票了'
 headers = {'Host': 'ticket.sstm.org.cn', 'Connection': 'keep-alive',
-           'mpsessid': '',
+           'mpsessid': mpsessid,
            'content-type': 'application/x-www-form-urlencoded', 'Accept-Encoding': 'gzip,compress,br,deflate',
-           'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.32(0x18002033) NetType/3G Language/zh_CN',
+           'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                         'like Gecko) Mobile/15E148 MicroMessenger/8.0.32(0x18002033) NetType/3G Language/zh_CN',
            'Referer': 'https://servicewechat.com/wx1d7ddce169710ba7/42/page-frame.html'}
-url = 'https://ticket.sstm.org.cn/vendor/reserve/getReservePeriodListByDate.xhtml?stadiumId=69001&reservedate=2023-04-28&appId=wx1d7ddce169710ba7'
+url = 'https://ticket.sstm.org.cn/vendor/reserve/getReservePeriodListByDate.xhtml?stadiumId=69001&reservedate=2023-04' \
+      '-28&appId=wx1d7ddce169710ba7'
 while True:
     r = requests.get(url, headers=headers)
     if r.status_code != 200:
@@ -25,7 +32,7 @@ while True:
             if data['data']['reservePeriodList'][0]['avaiablenum'] > 0 or data['data']['reservePeriodList'][1][
                 'avaiablenum'] > 0 or data['data']['reservePeriodList'][0]['availableNum'] > 0 or \
                     data['data']['reservePeriodList'][1]['availableNum'] > 0:
-                requests.get('https://api.day.app/<barkToken>/有票了')
+                requests.get(barkUrl)
                 print(time.asctime(time.localtime(time.time())), " ticket available")
             else:
                 print(time.asctime(time.localtime(time.time())), " ticket unavailable")
